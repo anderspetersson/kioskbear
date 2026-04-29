e5bd06bdfd2cce9af269304c472d2b39e5db0861
Anders Petersson <me@anderspetersson.se>
2026-04-29 21:22:40 +0200

    DJANGO_SECRET_KEY is not a tuple.

--- kioskbear_web/kioskbear/conf/settings/base.py
+++ kioskbear_web/kioskbear/conf/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (os.environ.get("DJANGO_SECRET_KEY"),)
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 AUTH_USER_MODEL = "accounts.Account"
 LOGIN_REDIRECT_URL = "app:dashboard"
